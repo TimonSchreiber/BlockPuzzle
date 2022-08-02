e376a0a03d3a936da19962aa58228f4c11f222b5
Timon Schreiber <timon.schreiber@live.de>
2022-08-02 17:46:22 +0200

    Added Instances for various Move Patterns.

--- src/field/MovePattern.java
+++ src/field/MovePattern.java
@@ -1,75 +1,116 @@
 package field;
 
+import java.util.Iterator;
 import java.util.List;
 
-public final class MovePattern {
+public final class MovePattern implements Iterable<Direction> {
 
     // -------------------------------------------------------------------------
     // STATIC ATTRIBUTES
     // -------------------------------------------------------------------------
 
-    // -------------------------------------------------------------------------
-    // Empty Direction List
-
-    public static final
-    List<Direction> NO_DIRECTIONS =
-        List.of();
+    /** {@code List} of {@code Directions} */
+    private final List<Direction> directions;
 
     // -------------------------------------------------------------------------
-    // Only Down and Up
+    // CONSTRUCTOR
+    // -------------------------------------------------------------------------
 
+    /** TODO
+     * Private constructor.
+     */
+    private MovePattern(List<Direction> directions) {
+        this.directions = directions;
+    }
+
+
+    // -------------------------------------------------------------------------
+    // INSTANCES
+    // -------------------------------------------------------------------------
+    
+    /** Empty Direction List */
     public static final
-    List<Direction> DOWN_UP_DIRECTIONS =
-        List.of(
+    MovePattern NO_DIRECTIONS =
+        new MovePattern(
+            List.of()
+        );
+
+    /** Only Down and Up Directions*/
+    public static final
+    MovePattern DOWN_UP_DIRECTIONS =
+        new MovePattern(
+            List.of(
             Direction.D,
             Direction.U
+            )
         );
-
-    // -------------------------------------------------------------------------
-    // Only Right and Left
-
+        
+    /** Only Right and Left Directions */
     public static final
-    List<Direction> RIGHT_LEFT_DIRECTIONS =
-        List.of(
-            Direction.R,
-            Direction.L
+    MovePattern RIGHT_LEFT_DIRECTIONS =
+        new MovePattern(
+            List.of(
+                Direction.R,
+                Direction.L
+            )
         );
-
-    // -------------------------------------------------------------------------
-    // Right, Down, Left, and Up
-
+        
+    /** Right, Down, Left, and Up */
     public static final
-    List<Direction> ALL_DIRECTIONS =
-        List.of(
-            Direction.R,
-            Direction.D,
-            Direction.L,
-            Direction.U
+    MovePattern ALL_DIRECTIONS =
+        new MovePattern(
+            List.of(
+                Direction.R,
+                Direction.D,
+                Direction.L,
+                Direction.U
+            )
         );
+
+
 
     // -------------------------------------------------------------------------
     // Two Steps in every possible and useful combination of Directions
+    // TODO: Make this into a MovePattern.
+    // not working because this is a'List of List of Directions' instead of a 'List of Directions'
 
-    public static final
-    List<List<Direction>> ALL_DIRECTIONS_PLUS =
-        List.of(
-            List.of(Direction.R),
-            List.of(Direction.D),
-            List.of(Direction.L),
-            List.of(Direction.U),
-            List.of(Direction.R, Direction.R),
-            List.of(Direction.R, Direction.D),
-            List.of(Direction.R, Direction.U),
-            List.of(Direction.D, Direction.R),
-            List.of(Direction.D, Direction.D),
-            List.of(Direction.D, Direction.L),
-            List.of(Direction.L, Direction.D),
-            List.of(Direction.L, Direction.L),
-            List.of(Direction.L, Direction.U),
-            List.of(Direction.U, Direction.R),
-            List.of(Direction.U, Direction.L),
-            List.of(Direction.U, Direction.U)
-        );
+    // public static final
+    // List<List<Direction>> ALL_DIRECTIONS_PLUS =
+    //     List.of(
+    //         List.of(Direction.R),
+    //         List.of(Direction.D),
+    //         List.of(Direction.L),
+    //         List.of(Direction.U),
+    //         List.of(Direction.R, Direction.R),
+    //         List.of(Direction.R, Direction.D),
+    //         List.of(Direction.R, Direction.U),
+    //         List.of(Direction.D, Direction.R),
+    //         List.of(Direction.D, Direction.D),
+    //         List.of(Direction.D, Direction.L),
+    //         List.of(Direction.L, Direction.D),
+    //         List.of(Direction.L, Direction.L),
+    //         List.of(Direction.L, Direction.U),
+    //         List.of(Direction.U, Direction.R),
+    //         List.of(Direction.U, Direction.L),
+    //         List.of(Direction.U, Direction.U)
+    //     );
+
+    
+    // =========================================================================
+    // INTERFACE - METHODS
+    // =========================================================================
+
+    // -------------------------------------------------------------------------
+    // ITERABLE
+    // -------------------------------------------------------------------------
+
+    /**
+     * Returns an {@code Iterator} over all {@code Directions}.
+     */
+    @Override
+    public Iterator<Direction> iterator() {
+        return this.directions.iterator();
+    }
 
     // =========================================================================
 
