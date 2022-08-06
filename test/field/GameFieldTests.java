0c0f8ce0e522c9e68100d50cfd37208ba3c57f58
Timon Schreiber <timon.schreiber@live.de>
2022-08-06 20:32:53 +0200

    Created a Test class for 'GameFIeld'.

---
+++ test/field/GameFieldTests.java
@@ -0,0 +1,30 @@
+package field;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GameFieldTests {
+
+    // -------------------------------------------------------------------------
+    // Constructor
+
+    // TODO: Tests here ?
+
+    // -------------------------------------------------------------------------
+    // Getter
+
+    // -------------------------------------------------------------------------
+    // Forwarding
+
+    // -------------------------------------------------------------------------
+    // equals
+
+    @Test
+    @DisplayName("text here")
+    public void test() {
+        assertEquals("expected", "actual");
+    }
+
+}   // Game Field Test class
