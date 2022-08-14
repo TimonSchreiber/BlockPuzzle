3c5eae3f0685699bb9aac19e59178dd79112ff14
Timon Schreiber <timon.schreiber@live.de>
2022-08-14 19:58:37 +0200

    Cleaned up some comments.

--- src/block/BlockInfo.java
+++ src/block/BlockInfo.java
@@ -2,22 +2,14 @@ package block;
 
 import java.awt.Color;
 
-// import field.Direction;
 import field.MovePattern;
 
-// --> old record fields <--
-// (Position position, int size, Direction direction)
+/**
+ * Holds all the information needed to create a new Block.
+ */
 public record BlockInfo(String name,
                         Color color,
                         MovePattern movePattern,
                         boolean isMainBlock,
                         PositionsInfo positionsInfo) {
-    // TODO:
-    // maybe change size to Name, have two constructors:
-    //      - one with name for StartingPositions
-    //      - one with size and automatic naming for manual game creation
-
-    // BlockInfo(Position position, String name, Direction direction)   (Alternative 1)
-    // BlockInfo(PositionList positionList, String name)                (Alternative 2)
-
 }   // BlockInfo record
