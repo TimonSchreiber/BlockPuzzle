f1a2c3227cad276c5ebdb84457be0bd203a1cda9
Timon Schreiber <timon.schreiber@live.de>
2022-08-14 20:28:50 +0200

    Added comments.

--- src/block/PositionsInfo.java
+++ src/block/PositionsInfo.java
@@ -2,9 +2,11 @@ package block;
 
 import field.Direction;
 
+/**
+ * Holds all the information needed for creating the PositionList of a Block.
+ */
 public record PositionsInfo(Position position,
                             int size,
                             Direction direction,
                             boolean isElbow) {
-    
-}
+}   // Positions Info record
