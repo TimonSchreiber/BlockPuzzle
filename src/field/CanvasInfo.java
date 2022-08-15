35fde5a095981a6a045af4b91a1048cc4db41487
Timon Schreiber <timon.schreiber@live.de>
2022-08-15 16:59:22 +0200

    New record holding information related to GameField#draw().

---
+++ src/field/CanvasInfo.java
@@ -0,0 +1,15 @@
+package field;
+
+import java.awt.Color;
+import java.util.List;
+
+/**
+ * A class for holding all the information needed for the canvas to draw on.
+ */
+public record CanvasInfo(
+    Color outsideColor,
+    Color insideColor,
+    Color winColor,
+    List<Rectangle> winArea
+) {
+}   // Canvas Info record
---
+++ src/field/Rectangle.java
@@ -0,0 +1,12 @@
+package field;
+
+/**
+ * Holds four double values for drawing rectangles.
+ */
+public record Rectangle(
+    double minX,
+    double minY,
+    double width,
+    double height
+) {
+}   // Rectangle record
