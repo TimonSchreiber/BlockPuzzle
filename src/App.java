57f64716e3a281a4af55f82ea48568b560c34165
Timon Schreiber <timon.schreiber@live.de>
2022-09-08 12:23:50 +0200

    Extracted Main code into separate methods.

--- src/App.java
+++ src/App.java
@@ -13,16 +13,20 @@ import solver.ManualMode;
  * This App provides three different Logic-Puzzles and also four different
  * ways for solving.
  *
- * Puzzles:
- *  - DirtyDozen    - 13 Games in total (number 0 to 12)
- *  - JumpinRabbits -  6 Games in total (number 0 to  5)
- *  - RushHour      -  4 Games in total (number 0 to  3)
+ * <p>Puzzles:
+ * <ul>
+ *   <li>DirtyDozen    - 13 Games in total (number 0 to 12)
+ *   <li>JumpinRabbits -  6 Games in total (number 0 to  5)
+ *   <li>RushHour      -  4 Games in total (number 0 to  3)
+ * </ul>
  *
- * solving methods:
- *  - Depth First Search
- *  - Breadth First Search
- *  - Breadth fFirst Search with Multi-Threads
- *  - Manual Mode
+ * Solving Methods:
+ * <ul>
+ *  <li>Depth First Search
+ *  <li>Breadth First Search
+ *  <li>Breadth fFirst Search with Multi-Threads
+ *  <li>Manual Mode
+ * </ul>
  *
  * @version     1.1     16 Aug 2022
  * @author      Timon Schreiber
@@ -31,63 +35,98 @@ public class App {
 
     public static void main(String[] args) {
 
+        // ---------------------------------------------------------------------
+        // Variables
+
+        /** D=DirtyDozen; J=HumpingRabbits; R=RushHour */
+        final String gameType;
+
+        /** D=Depth; B=Breadth; T=Threads; M=Manual */
+        final String solverType;
+
+        /** The key of the games' BlockInfo-Map */
+        final int gameNumber;
+
+        /** Time in milliseconds between two moves */
+        final int delay;
+
+        /** The Game to play */
+        final Game game;
+
         try (final Scanner scanner = new Scanner(System.in)) {
-            final String gameType   = "D";  // [D=DirtyDozen; J=HumpingRabbits; R=RushHour]
-            final String solvType   = "B";  // [D=Depth; B=Breadth; T=Threads; M=Manual]
-            final int gameNumber    = 12;
-            final int delay         = 200;
+            gameType    = "D";
+            solverType  = "B";
+            gameNumber  = 12;
+            delay       = 200;
 
-            System.out.println("\nGame Number: " + gameNumber);
+            game = getGame(gameType.toUpperCase().charAt(0), gameNumber);
 
-            final Game game =
-                switch (gameType.toUpperCase().charAt(0)) {
-                    case 'D' -> new DirtyDozen(     gameNumber);
-                    case 'J' -> new JumpingRabbits( gameNumber);
-                    case 'R' -> new RushHour(       gameNumber);
-                    default  -> throw new IllegalStateException("Invalid GameType: " + gameType);
-                };
+            printGameNumber(gameNumber);
 
-            System.out.println("\nBoard:");
-            game.print();
+            printBoard(game);
 
-            switch (solvType.toUpperCase().charAt(0)) {
-                case 'D' -> new DepthFirstSearch(   game, (delay/10), false ).solve();
-                case 'B' -> new BreadthFirstSearch( game,  delay            ).solve();
-                case 'T' -> new BFS_WithThreads(    game,  delay            ).solve();
-                case 'M' -> new ManualMode(         game,  delay,   scanner ).play();
-                default  -> throw new IllegalStateException("Invalid SolveType: " + solvType);
-            };
+            solve(solverType, game, delay, scanner);
         }
 
-        // ---------------------------------------------------------------------
-
-        // switch (args.length) {
-        // case 3:
-        //     type = args[0];
-        //     number = Integer.parseInt(args[1]);
-        //     delay = Integer.parseInt(args[2]);
-        //     show = Boolean.parseBoolean(args[3]);
-        //     break;
-        // default:
-        //     // Enter Solve Type
-        //     System.out.println("Depth- or Breadth-first search? [D/B]");
-        //     type = sc.nextLine().toUpperCase();
-
-        //     // Enter Game Number
-        //     System.out.println("Enter the Game-Number you want to play:");
-        //     number = sc.nextInt();
-
-        //     // Enter Delay
-        //     System.out.println("Enter the time you want to see the boeard [ms]:");
-        //     delay = sc.nextInt();
-
-        //     // Enter Show
-        //     System.out.println("Enter if you want to see the board while it is solved [true/false]:");
-        //     show = sc.nextBoolean();
-        //     break;
-        // }
-
         return;
-    }   // end Main
+    }   // Main
 
-}   // end class
+    // ---------------------------------------------------------------------
+
+    /**
+     * Print the board of the Game on the console.
+     *
+     * @param game  The Game
+     */
+    private static void printBoard(Game game) {
+        System.out.println("\nBoard:");
+        game.print();
+        return;
+    }
+
+    /**
+     * Print the GameNumber on the console.
+     *
+     * @param gameNumber    the game number
+     */
+    private static void printGameNumber(int gameNumber) {
+        System.out.println("\nGame Number: " + gameNumber);
+        return;
+    }
+
+    /**
+     * Call the #solve()/#play() method depending on the solveType.
+     *
+     * @param solvType  The solver type
+     * @param game      The Game
+     * @param delay     The Time delay
+     * @param scanner   The Scanner
+     */
+    private static void solve(String solvType, Game game, int delay, Scanner scanner) {
+        switch (solvType.toUpperCase().charAt(0)) {
+            case 'D' -> new DepthFirstSearch(   game, (delay/10), false ).solve();
+            case 'B' -> new BreadthFirstSearch( game,  delay            ).solve();
+            case 'T' -> new BFS_WithThreads(    game,  delay            ).solve();
+            case 'M' -> new ManualMode(         game,  delay,   scanner ).play();
+            default  -> throw new IllegalStateException("Invalid SolveType: " + solvType);
+        };
+        return;
+    }
+
+    /**
+     * Returns the Game depending on the game type.
+     *
+     * @param gameType      The game type
+     * @param gameNumber    The game number
+     * @return              Returns a new instance of the specified Game class.
+     */
+    private static Game getGame(char gameType, int gameNumber) {
+        return switch (gameType) {
+            case 'D' -> new DirtyDozen(     gameNumber);
+            case 'J' -> new JumpingRabbits( gameNumber);
+            case 'R' -> new RushHour(       gameNumber);
+            default  -> throw new IllegalStateException("Invalid GameType: " + gameType);
+        };
+    }
+
+}
